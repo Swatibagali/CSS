0e03c3ed159d04dbc16591dadd7564c4eb75c3df
Swatibagali <152793636+Swatibagali@users.noreply.github.com>
2026-01-14 12:06:52 +0530

    Add files via upload
    NO CHANGES

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+var a = "swati";
+var b = 1;
+var c = true;
+var d = [1,2,3,4,5];
+var e = 1.2;
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+
